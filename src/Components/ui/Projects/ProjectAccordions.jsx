37d8aac40e8c7a3b0b2fad85340f0987248108f4
Ahsan Khan <mahsankh99@gmail.com>
2024-11-09 01:18:55 +0500

    Update ProjectAccordions.jsx

--- src/Components/ui/Projects/ProjectAccordions.jsx
+++ src/Components/ui/Projects/ProjectAccordions.jsx
@@ -3,7 +3,7 @@ import {
     AccordionContent,
     AccordionItem,
     AccordionTrigger,
-  } from "@/components/ui/accordionprojects"
+  } from "../accordionprojects"
 import React from 'react'
 
 const ProjectAccordions = () => {

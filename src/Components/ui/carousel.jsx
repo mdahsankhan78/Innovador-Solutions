5ff8dfd661864b2b8335da1d202a9e98a48f6ddb
Ahsan Khan <mahsankh99@gmail.com>
2024-11-09 01:21:59 +0500

    Update carousel.jsx

--- src/Components/ui/carousel.jsx
+++ src/Components/ui/carousel.jsx
@@ -3,7 +3,7 @@ import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons"
 import useEmblaCarousel from "embla-carousel-react";
 
 import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { Button } from "./button"
 
 const CarouselContext = React.createContext(null)
 

59bab2d06236e0a94a8929a037d5f262989f0cf1
Muhammad Ahsan Khan <mahsankh99@gmail.com>
2024-12-04 13:03:25 +0500

    Update carousel.jsx

--- src/Components/ui/carousel.jsx
+++ src/Components/ui/carousel.jsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import useEmblaCarousel from "embla-carousel-react";
 
 import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { Button } from "../button"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
